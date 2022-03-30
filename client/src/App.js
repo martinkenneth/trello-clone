7ca4cae7674d23c10daadc14d705ac1a064e3404
RochaThomas <tomrocha99@gmail.com>
2022-03-30 13:09:23 -0700

    trying again

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import ProjectBoard from "./views/ProjectBoard";
 function App() {
     return (
         <div className="App">
-            <h1>Testing Again</h1>
+            <h1>We re finally getting this</h1>
             <h1>Changing app.js within feature branch</h1>
             <ProjectBoard />
         </div>
