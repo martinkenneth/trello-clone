5f08612971482b99562a8b5509273ec0d2e92e54
Kenneth Martin <martinckenneth@gmail.com>
2022-03-31 02:54:24 -0700

    init server folder

---
+++ server/server.js
@@ -0,0 +1,27 @@
+/*===========================================================================
+    Express Server and Dependency Setup
+===========================================================================*/
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = 8000;
+const DB = "test";
+
+/*===========================================================================
+    Middleware 
+===========================================================================*/
+app.use(express.json(), express.urlencoded({ extended: true }), cors());
+
+/*===========================================================================
+    DB Connection 
+===========================================================================*/
+// This will fire our mongoose.connect statement to initialize our db connection
+require("./config/mongoose.config")(DB);
+
+/*===========================================================================
+    Import Server Routes
+===========================================================================*/
+/* --- User Routes --- */
+
+// Test to see if server is running upon setup...
+app.listen(PORT, () => console.log("The server is all fired up on port 8000"));
