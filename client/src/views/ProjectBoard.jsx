30b67cd1019b8499c20a4ba4587b9fa67571aed9
Kenneth Martin <martinckenneth@gmail.com>
2022-03-30 20:55:22 -0700

    push current updates/ pull most updated origin

--- client/src/views/ProjectBoard.jsx
+++ client/src/views/ProjectBoard.jsx
@@ -1,34 +1,35 @@
-import React, {useState} from 'react'
-import ListColumn from '../components/ListColumn';
-import { projName, lists } from '../data/mock-data';
-import style from './ProjectBoardView.module.css';
-import AddListForm from '../components/AddListForm';
+import React, { useState } from "react";
+import NavigationBar from "../components/NavigationBar";
+import ListColumn from "../components/ListColumn";
+import AddListForm from "../components/AddListForm";
+import style from "./ProjectBoardView.module.css";
+import { projName, lists } from "../data/mock-data";
 
 const ProjectBoard = () => {
-
     const [addList, setAddList] = useState(false);
 
     const handleAddList = () => {
         setAddList(true);
-    }
+    };
 
     return (
         <div className={style.projectBoard}>
-            <div className={style.navBar}>
+            {/* <div className={style.navBar}>
                 <h1>{projName}</h1>
-            </div>
+            </div> */}
             <div className={style.columnsContainer}>
                 {lists.map((list, idx) => {
-                    return (
-                        <ListColumn key={idx} listTitle={list.title}/>
-                    )
+                    return <ListColumn key={idx} listTitle={list.title} />;
                 })}
 
-                {addList ? <AddListForm setAddList={setAddList}/>:<button onClick={handleAddList}>Add a List</button>}
-                
+                {addList ? (
+                    <AddListForm setAddList={setAddList} />
+                ) : (
+                    <button onClick={handleAddList}>Add a List</button>
+                )}
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default ProjectBoard;
