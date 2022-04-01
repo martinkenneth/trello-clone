25a32aefee7e3fd7153b2c3e60847819688c9690
Kenneth Martin <martinckenneth@gmail.com>
2022-03-31 23:10:39 -0700

    update

--- client/src/views/ProjectBoard.jsx
+++ client/src/views/ProjectBoard.jsx
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from "react";
 import ListColumn from "../components/ListColumn";
-import style from "./ProjectBoardView.module.css";
 import AddListForm from "../components/AddListForm";
+import Modal from "../components/Modal";
 import EditOne from "./EditOne";
+import style from "./ProjectBoardView.module.css";
 import plus from "../assets/plus-icon.png";
 import axios from "axios";
 
 const ProjectBoard = () => {
     const [addList, setAddList] = useState(false);
     const [showId, setShowId] = useState(-1);
-
     const [lists, setLists] = useState([]);
+
+    // const [openModal, setOpenModal] = useState(false);
 
     const handleAddList = () => {
         setAddList(true);
@@ -21,19 +23,22 @@ const ProjectBoard = () => {
             .get("http://localhost:8000/api/lists")
             .then((res) => {
                 console.log("getting the lists from the db");
+                // console.log(showId);
                 setLists(res.data.lists);
             })
             .catch((err) => console.error(err));
-    }, [addList]);
+    }, [addList, showId]);
 
     return (
         <div className={style.projectBoard}>
+            {showId !== -1 && <EditOne showId={showId} setShowId={setShowId} />}
             <div className={style.columnsContainer}>
                 {lists.map((list) => {
                     return (
                         <ListColumn
                             key={list._id}
                             listTitle={list.title}
+                            showId={showId}
                             setShowId={setShowId}
                         />
                     );
@@ -48,7 +53,7 @@ const ProjectBoard = () => {
                     </button>
                 )}
             </div>
-            {showId !== -1 && <EditOne showId={showId} setShowId={setShowId} />}
+            {/* <Modal /> */}
         </div>
     );
 };
